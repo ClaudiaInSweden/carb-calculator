2221bfd6807e6a0a25a6b570f7f529d73496b8a7
Claudia Bergstaller <claudia.bergstaller@gmx.com>
2023-10-31 13:12:07 +0000

    Add link to google spreadsheet for user to open

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+import webbrowser
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -16,4 +17,14 @@ compare = SHEET.worksheet('compare')
 
 data = compare.get_all_values()
 
-print(data)
+print("\nWelcome to the carbohydrate calculator for cat food!\n")
+print("Most cat foods don't have the carb content listed but")
+print("for obese and diabetic cats it's important to know")
+print("how much carbs their food contains.\n") 
+
+
+
+
+print("To see the compilation of the cat food you entered, use (ctrl + click) to open the link:")
+
+print('\033]8;;https://docs.google.com/spreadsheets/d/1nvKyRNOHlLlbLtARXVFYgILkAPpbYJ-Ywku7EwrQzts/edit?usp=sharing/\033\\Open the comparison\033]8;;\033\\\n')
