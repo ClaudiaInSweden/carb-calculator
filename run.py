a425fcab154c087f0e8db7f6acb57419cb5ebe4d
Claudia Bergstaller <claudia.bergstaller@gmx.com>
2023-11-01 18:26:11 +0000

    Add user choice to say yes or no to further data input

--- run.py
+++ run.py
@@ -33,27 +33,35 @@ def get_user_input():
     print("using dot as decimal separator")
     print("Example: Crude Protein/Protein in %: 12.5")
 
-    protein = float(input("Enter crude protein/protein in %:\n"))
-    fat = float(input("Enter crude fat/fat in %:\n"))
-    fiber = float(input("Enter crude fiber/fiber in %:\n"))
-    ash = float(input("Enter ash in %:\n"))
-    moisture = float(input("Enter moisture in %:\n"))
-    carbs = 100 - (protein + fat + fiber + ash + moisture)
-    print(f"The cat food contains {carbs} % carbs.")
+    data = []
+
+    while True:
+
+        protein = float(input("Enter crude protein/protein in %:\n"))
+        fat = float(input("Enter crude fat/fat in %:\n"))
+        fiber = float(input("Enter crude fiber/fiber in %:\n"))
+        ash = float(input("Enter ash in %:\n"))
+        moisture = float(input("Enter moisture in %:\n"))
+
+        data.append(protein + fat + fiber + ash + moisture)
+
+        carbs = 100 - (protein + fat + fiber + ash + moisture)
+        data.append(carbs)
+
+        print(f"The cat food contains {carbs} % carbs.")
+
+        choice = input("Would you like to check another product? ( y / n ) : ")
+        if choice.casefold() == 'n':
+            break
+
 get_user_input()
 
 
 def validate_input(values):
     try:
-        [float(value) for value in values]
-        round(value, 1)
-        if value == "":
-            raise ValueError(
-                print("Please enter a valid figure.")
-            )
-
-    except TypeError as e:
-        print(f"Invalid data: {e}, please try again!\n")
+        protein, fat, fiber, ash, moisture = str(protein, fat, fiber, ash, moisture)
+    except:
+        print("Please enter a valid number.")
         return False
 
     return True
