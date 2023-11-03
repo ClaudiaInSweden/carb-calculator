4321715f2f62f7d3e4abe95e20b690e4c234be1c
Claudia Bergstaller <claudia.bergstaller@gmx.com>
2023-11-03 12:07:53 +0000

    Add __main__ top-level code and clean up code

--- run.py
+++ run.py
@@ -1,95 +1,57 @@
-""" 
-Import libraries and connect to Google spreadsheets
-"""
-import gspread
-from google.oauth2.service_account import Credentials
-import webbrowser
-
-SCOPE = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
-    ]
-
-CREDS = Credentials.from_service_account_file('creds.json')
-SCOPED_CREDS = CREDS.with_scopes(SCOPE)
-GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('carb-calculator')
-
-compare = SHEET.worksheet('compare')
-
-data = compare.get_all_values()
-
-"""
-Information for user what this project is about incl. instructions
-"""
-print("\nWelcome to the carbohydrate calculator for cat food!\n")
-print("Most cat foods don't have the carb content listed but")
-print("for people with obese and diabetic cats it's important to know")
-print("how much carbs the food contains.\n") 
-print("Use this interface to calculate the carb content of dry cat food.\n")
-print("Please enter the respective percentage from your cat food label,")
-print("using dot as decimal separator")
-print("Example: Crude Protein/Protein in %: 12.5\n")
-
-"""
-Get food content data from user.
-Five inputs need to be done. Inputs are converted to floats
-and validated. A while loop runs until all five entered
-values are valid.
-"""
+def display_instructions():
+    """
+    Information for user what this project is about incl. instructions
+    """
+    print("\nWelcome to the carbohydrate calculator for cat food!\n")
+    print("Most cat foods don't have the carb content listed but")
+    print("for people with obese and diabetic cats it's important to know")
+    print("how much carbs the food contains.\n") 
+    print("Use this interface to calculate the carb content of dry cat food.\n")
+    print("Please enter the respective percentage from your cat food label,")
+    print("using dot as decimal separator")
+    print("Example: Crude Protein/Protein in %: 12.5\n")
 
 def get_user_input(var):
-   
+    """
+    Get food content data from user.
+    Five inputs need to be done. Inputs are converted to floats
+    and validated. A while loop runs until all five entered
+    values are valid.
+    """
     while True:
 
-        value = float(input("Enter {} in %: ".format(var)))
+        value = (input("Enter {} in %: ".format(var)))
         try:
-            if value > 99:
-                raise ValueError
-            elif value < 0:
-                raise ValueError
-
+            return float(value)
+            if 0 <= value >= 99:
+                raise ValueError("Please enter a positive number between 0 and 100.")
         except ValueError as e:
-            print(f"Value Error! Please enter a positive number between 0 and 100.\n")
-            return False
-
-        return True
-        
-
-
-
-protein = get_user_input("protein")
-fat = get_user_input("fat")
-fiber = get_user_input("fiber")
-ash = get_user_input("ash")
-moisture = get_user_input("moisture")
-
-"""
-We need the sum of the entered values to calculate the carb content
-"""
-subtotal = protein + fat + fiber + ash + moisture
-"""
-We calculate the carb content by subtracting the total of the 
-entered values from 100 percent and print the result of the
-calculation to the terminal.
-"""
+            print("Value Error! Please enter a positive number between 0 and 100.\n")
+            continue
+           
 def calculate_carbs(subtotal):
+    """
+    We calculate the carb content by subtracting the total of the 
+    entered values from 100 percent and print the result of the
+    calculation to the terminal.
+    """
     carbs = 100 - subtotal
-    print(f"The cat food contains {carbs} % carbs.")
+    print(f"\nThe cat food contains {carbs} % carbs.\n")
     return carbs
 
-carb_result = calculate_carbs(subtotal)
-food_content = [protein, fat, fiber, ash, moisture, carb_result]
-print(food_content)
+def calculate_carbs_in_cat_food():
+    protein = get_user_input("protein")
+    fat = get_user_input("fat")
+    fiber = get_user_input("fiber")
+    ash = get_user_input("ash")
+    moisture = get_user_input("moisture")
+    """
+    We need the sum of the entered values to calculate the carb content
+    """
+    subtotal = protein + fat + fiber + ash + moisture
+    carb_result = calculate_carbs(subtotal)
+    food_content = [protein, fat, fiber, ash, moisture, carb_result]
+    print(food_content)
 
-            
-# Add data to excel sheet not yet created!!!
-
-"""
-Enable user to see the comparison of the entered items
-"""
-print("\nTo see the compilation of the cat food you entered, use (ctrl + click) to open the link:")
-
-print('\033]8;;https://docs.google.com/spreadsheets/d/1nvKyRNOHlLlbLtARXVFYgILkAPpbYJ-Ywku7EwrQzts/edit?usp=sharing/\033\\Open the comparison\033]8;;\033\\\n')
-
+if __name__ == '__main__':
+    calculate_carbs_in_cat_food()
